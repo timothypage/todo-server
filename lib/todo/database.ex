090d31d3816f0ee5413fbc45480d3a6c3fc8af01
Tim Zwolak <Tim.Zwolak@charter.com>
2017-07-14 13:50:02 -0600

    add database workers

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -6,16 +6,52 @@ defmodule Todo.Database do
   end
 
   def store(key, data) do
-    GenServer.cast(:database_server, {:store, key, data})
+    key
+    |> choose_worker
+    |> Todo.DatabaseWorker.store(key, data)
   end
 
   def get(key) do
-    GenServer.call(:database_server, {:get, key})
+    key
+    |> choose_worker
+    |> Todo.DatabaseWorker.get(key)
+  end
+
+  def choose_worker(key) do 
+    GenServer.call(:database_server, {:choose_worker, key})
   end
 
   def init(db_folder) do
-    File.mkdir_p(db_folder)
-    {:ok, db_folder}
+    {:ok, start_workers(db_folder)}
+  end
+
+  defp start_workers(db_folder) do
+    for index <- 1..3, into: Map.new do
+      {:ok, pid} = Todo.DatabaseWorker.start(db_folder)
+      {index-1, pid}
+    end
+  end
+
+  def handle_call({:choose_worker, key}, _, workers) do
+    worker_key = :erlang.phash2(key, 3)
+
+    {:reply, Map.get(workers, worker_key), workers}
+  end
+end
+
+defmodule Todo.DatabaseWorker do
+  use GenServer
+
+  def start(db_folder) do
+    GenServer.start(__MODULE__, db_folder)
+  end
+
+  def store(worker_pid, key, data) do
+    GenServer.cast(worker_pid, {:store, key, data})
+  end
+
+  def get(worker_pid, key) do
+    GenServer.call(worker_pid, {:get, key})
   end
 
   def handle_cast({:store, key, data}, db_folder) do
