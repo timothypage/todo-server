e443a46904d2c0d0a113245b752d92a6bc8f5b98
Tim Zwolak <Tim.Zwolak@charter.com>
2017-07-15 11:18:06 -0600

    add via tuple to Todo.Server

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -3,7 +3,15 @@ defmodule Todo.Server do
 
   def start_link(name) do
     IO.puts("starting Todo.Server for #{name}")
-    GenServer.start_link(Todo.Server, name)
+    GenServer.start_link(Todo.Server, name, name: via_tuple(name))
+  end
+
+  defp via_tuple(name) do
+    {:via, Todo.ProcessRegistry, {:todo_server, name}}
+  end
+
+  def whereis(name) do
+    Todo.ProcessRegistry.whereis_name({:todo_server, name})
   end
 
   def add_entry(todo_server, new_entry) do
